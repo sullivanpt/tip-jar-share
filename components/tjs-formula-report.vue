dcb90002862d0d58f74940e44d076dc94be66db9
Patrick Sullivan <sullivanpt@acm.org>
2019-05-25 12:02:58 -0700

    refactor: print table component

---
+++ components/tjs-formula-report.vue
@@ -0,0 +1,192 @@
+<template>
+  <div class="print-table-wrap">
+    <table class="print-table">
+      <thead>
+        <tr>
+          <th
+            v-for="head in formulaReport.groupHeaders"
+            :key="head.key"
+            :class="head | classByHeader"
+          >
+            {{ head | titleByHeader }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <template v-for="(grp, grpIdx) in formulaReport.groups">
+          <!-- e.g. Position bartender -->
+          <tr
+            :key="`grp-head-${grp.allocationId}`"
+            class="print-table-subheader"
+          >
+            <td
+              :colspan="formulaReport.groupHeaders.length"
+              v-text="`${grp.position} position`"
+            />
+          </tr>
+          <!-- position reporters -->
+          <template v-for="rptr in formulaReport.groupedReporters[grpIdx]">
+            <tr :key="`rptr-${grp.allocationId}-${rptr.memberId}`">
+              <td
+                v-for="head in formulaReport.groupHeaders"
+                :key="head.key"
+                :class="head | classByHeader"
+              >
+                {{ rptr[head.key] | formatByHeader(head) }}
+              </td>
+            </tr>
+          </template>
+          <!-- position collections -->
+          <template v-for="col in formulaReport.groupedCollections[grpIdx]">
+            <tr :key="`col-${grp.allocationId}-${col.stationId}`">
+              <td
+                v-for="head in formulaReport.groupHeaders"
+                :key="head.key"
+                :class="head | classByHeader"
+              >
+                <span v-if="head.key === 'name'">
+                  <i>tip jar &mdash; </i>{{ col[head.key] }}</span
+                >
+                <span v-else>
+                  {{ col[head.key] | formatByHeader(head) }}
+                </span>
+              </td>
+            </tr>
+          </template>
+          <!-- position subtotals -->
+          <tr :key="`grp-sub-${grp.allocationId}`" class="print-table-subtotal">
+            <td
+              v-for="head in formulaReport.groupHeaders"
+              :key="head.key"
+              :class="head | classByHeader"
+            >
+              <span
+                v-if="head.key === 'name'"
+                v-text="`${grp.position} subtotals`"
+              />
+              <span v-else>{{
+                formulaReport.groupedSubtotals[grpIdx][head.key]
+                  | formatByHeader(head)
+              }}</span>
+            </td>
+          </tr>
+          <!-- spacer -->
+          <tr :key="`spacer-${grp.allocationId}`">
+            <td :colspan="formulaReport.groupHeaders.length">&nbsp;</td>
+          </tr>
+        </template>
+      </tbody>
+      <tfoot>
+        <tr class="print-table-total">
+          <td
+            v-for="head in formulaReport.groupHeaders"
+            :key="head.key"
+            :class="head | classByHeader"
+          >
+            <span v-if="head.key === 'name'">totals</span>
+            <span v-else>{{
+              formulaReport.groupedTotal[head.key] | formatByHeader(head)
+            }}</span>
+          </td>
+        </tr>
+      </tfoot>
+    </table>
+  </div>
+</template>
+
+<script>
+import { formatCurrency, formatHours, formatPercent } from '~/helpers/math.js'
+
+function titleByHeader(head) {
+  const map = {
+    name: 'name', // member or station name
+    hours: 'hours',
+    hoursWeight: '% hours',
+    salesTotal: 'gross sales',
+    salesExcluded: 'sales excluded',
+    salesNet: 'net sales',
+    salesNetWeight: '% net sales',
+    salesContribute: 'sales contributed',
+    tipsPos: 'POS tips',
+    tipsPosContribute: 'POS tips contributed',
+    tipsPosNet: 'POS tips held',
+    tipsPosPooled: 'total POS contributed',
+    tipsPosPooledA: 'intermediate shared POS',
+    tipsPosPooledB: 'remaining shared POS',
+    tipsPosShare: 'share of remaining POS',
+    tipsPosFinal: 'take home POS tips',
+    tipsCash: 'cash tips',
+    tipsCashContribute: 'cash tips contributed',
+    tipsCashNet: 'cash tips held',
+    tipsCashCollected: 'tip jar cash',
+    tipsCashPooled: 'total cash contributed',
+    tipsCashPooledA: 'intermediate shared cash',
+    tipsCashPooledB: 'remaining shared cash',
+    tipsCashShare: 'share of remaining cash',
+    tipsCashFinal: 'take home cash tips',
+    tipsFinal: 'total take home tips'
+  }
+  return map[head.key] || ''
+}
+
+function classByHeader(head) {
+  return head.format === 'string' ? 'print-table-label' : 'print-table-amount'
+}
+
+function formatByHeader(v, head) {
+  if (head.format === 'currency') return formatCurrency(v)
+  if (head.format === 'percent') return formatPercent(v)
+  if (head.format === 'hours') return formatHours(v)
+  return v
+}
+
+/**
+ * print a portion of a formula-report
+ */
+export default {
+  filters: { classByHeader, formatByHeader, titleByHeader },
+  props: {
+    formulaReport: { type: Object, default: () => ({}) }
+  }
+}
+</script>
+
+<style>
+.print-table-wrap {
+  overflow-x: auto;
+}
+
+.print-table {
+  border-collapse: collapse;
+}
+
+.print-table th,
+.print-table td {
+  border: 1px solid darkgray;
+  padding-left: 5px;
+  padding-right: 5px;
+}
+
+.print-table-label {
+  min-width: 20em;
+  text-align: left;
+}
+
+.print-table-amount {
+  min-width: 4em;
+  text-align: right;
+}
+
+.print-table-subheader {
+  font-style: italic;
+}
+
+.print-table-subtotal {
+  font-style: italic;
+  font-weight: bold;
+}
+
+.print-table-total {
+  font-weight: bold;
+}
+</style>
--- pages/reports/daily/_reportId/print.vue
+++ pages/reports/daily/_reportId/print.vue
@@ -2,196 +2,59 @@
   <v-container pa-0 grid-list-lg>
     <v-layout column>
       <v-flex>
-        <div class="text-xs-right hidden-print">
+        <div class="text-xs-right hidden-print-only">
           <v-btn @click="back"><v-icon>arrow_back</v-icon> back</v-btn>
           <v-btn @click="print"><v-icon>print</v-icon> print</v-btn>
         </div>
-        <div class="text-xs-right font-italic">
+        <div class="text-xs-right caption font-italic">
           <div>generated in {{ applicationTitle }} {{ gitRepoVersion }}</div>
-          <div>team {{ organizationVersion }}</div>
-          <div>sharing method {{ formulaVersion }}</div>
-          <div>report {{ reportVersion }}</div>
         </div>
-        <div>Team: <span v-text="organization.name" /></div>
-        <div>Date: {{ report.date | formatDate }}</div>
+        <div>
+          Team: <span v-text="organization.name" />
+          <span class="caption font-italic">
+            &mdash; {{ organizationVersion }}
+          </span>
+        </div>
+        <div>
+          Date: <b>{{ report.date | formatDate }}</b>
+          <span class="caption font-italic">&mdash; {{ reportVersion }}</span>
+        </div>
         <div>Status: <span v-text="report.status" /></div>
         <div v-if="errors">
           <v-icon>error</v-icon> WARNING: DETECTED AN INCORRECT SETUP
         </div>
       </v-flex>
       <v-flex v-if="formulaReport">
-        <div class="print-table-wrap">
-          <table class="print-table">
-            <thead>
-              <tr>
-                <th
-                  v-for="head in formulaReport.groupHeaders"
-                  :key="head.key"
-                  :class="head | classByHeader"
-                >
-                  {{ head | titleByHeader }}
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <template v-for="(grp, grpIdx) in formulaReport.groups">
-                <!-- e.g. Position bartender -->
-                <tr
-                  :key="`grp-head-${grp.allocationId}`"
-                  class="print-table-subheader"
-                >
-                  <td
-                    :colspan="formulaReport.groupHeaders.length"
-                    v-text="`${grp.position} position`"
-                  />
-                </tr>
-                <!-- position reporters -->
-                <template
-                  v-for="rptr in formulaReport.groupedReporters[grpIdx]"
-                >
-                  <tr :key="`rptr-${grp.allocationId}-${rptr.memberId}`">
-                    <td
-                      v-for="head in formulaReport.groupHeaders"
-                      :key="head.key"
-                      :class="head | classByHeader"
-                    >
-                      {{ rptr[head.key] | formatByHeader(head) }}
-                    </td>
-                  </tr>
-                </template>
-                <!-- position collections -->
-                <template
-                  v-for="col in formulaReport.groupedCollections[grpIdx]"
-                >
-                  <tr :key="`col-${grp.allocationId}-${col.stationId}`">
-                    <td
-                      v-for="head in formulaReport.groupHeaders"
-                      :key="head.key"
-                      :class="head | classByHeader"
-                    >
-                      <span v-if="head.key === 'name'">
-                        <i>tip jar -- </i>{{ col[head.key] }}</span
-                      >
-                      <span v-else>
-                        {{ col[head.key] | formatByHeader(head) }}
-                      </span>
-                    </td>
-                  </tr>
-                </template>
-                <!-- position subtotals -->
-                <tr
-                  :key="`grp-sub-${grp.allocationId}`"
-                  class="print-table-subtotal"
-                >
-                  <td
-                    v-for="head in formulaReport.groupHeaders"
-                    :key="head.key"
-                    :class="head | classByHeader"
-                  >
-                    <span
-                      v-if="head.key === 'name'"
-                      v-text="`${grp.position} subtotals`"
-                    />
-                    <span v-else>{{
-                      formulaReport.groupedSubtotals[grpIdx][head.key]
-                        | formatByHeader(head)
-                    }}</span>
-                  </td>
-                </tr>
-                <!-- spacer -->
-                <tr :key="`spacer-${grp.allocationId}`">
-                  <td :colspan="formulaReport.groupHeaders.length">&nbsp;</td>
-                </tr>
-              </template>
-            </tbody>
-            <tfoot>
-              <tr class="print-table-total">
-                <td
-                  v-for="head in formulaReport.groupHeaders"
-                  :key="head.key"
-                  :class="head | classByHeader"
-                >
-                  <span v-if="head.key === 'name'">totals</span>
-                  <span v-else>{{
-                    formulaReport.groupedTotal[head.key] | formatByHeader(head)
-                  }}</span>
-                </td>
-              </tr>
-            </tfoot>
-          </table>
-        </div>
+        <tjs-formula-report :formula-report="formulaReport" />
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
+import { formatDate } from '~/helpers/time'
 import { nuxtPageNotFound } from '~/helpers/nuxt'
 import { applicationTitle, gitRepoVersion } from '~/helpers/site-map.js'
-import { formulaFindById, formulaGetVersion } from '~/helpers/formulas'
+import { formulaFindById } from '~/helpers/formulas'
 import { reportDaily } from '~/helpers/formulas-reports/daily'
 import {
   organizationFindById,
   organizationGetVersion
 } from '~/helpers/organizations'
 import { reportFindById, reportGetVersion } from '~/helpers/reports'
-import { formatDate } from '~/helpers/time'
-import { formatCurrency, formatHours, formatPercent } from '~/helpers/math.js'
 import { print } from '~/helpers/browser'
-
-function titleByHeader(head) {
-  const map = {
-    name: 'name', // member or station name
-    hours: 'hours',
-    hoursWeight: '% hours',
-    salesTotal: 'gross sales',
-    salesExcluded: 'sales excluded',
-    salesNet: 'net sales',
-    salesNetWeight: '% net sales',
-    salesContribute: 'sales contributed',
-    tipsPos: 'POS tips',
-    tipsPosContribute: 'POS tips contributed',
-    tipsPosNet: 'POS tips held',
-    tipsPosPooled: 'total POS contributed',
-    tipsPosPooledA: 'intermediate shared POS',
-    tipsPosPooledB: 'remaining shared POS',
-    tipsPosShare: 'share of remaining POS',
-    tipsPosFinal: 'take home POS tips',
-    tipsCash: 'cash tips',
-    tipsCashContribute: 'cash tips contributed',
-    tipsCashNet: 'cash tips held',
-    tipsCashCollected: 'tip jar cash',
-    tipsCashPooled: 'total cash contributed',
-    tipsCashPooledA: 'intermediate shared cash',
-    tipsCashPooledB: 'remaining shared cash',
-    tipsCashShare: 'share of remaining cash',
-    tipsCashFinal: 'take home cash tips',
-    tipsFinal: 'total take home tips'
-  }
-  return map[head.key] || ''
-}
-
-function classByHeader(head) {
-  return head.format === 'string' ? 'print-table-label' : 'print-table-amount'
-}
-
-function formatByHeader(v, head) {
-  if (head.format === 'currency') return formatCurrency(v)
-  if (head.format === 'percent') return formatPercent(v)
-  if (head.format === 'hours') return formatHours(v)
-  return v
-}
+import TjsFormulaReport from '~/components/tjs-formula-report'
 
 export default {
   layout: 'print',
-  filters: { classByHeader, formatByHeader, formatDate, titleByHeader },
+  components: { TjsFormulaReport },
+  filters: { formatDate },
   data: () => ({
     applicationTitle,
     gitRepoVersion,
     report: null,
     reportVersion: null,
     formula: null,
-    formulaVersion: null,
     formulaReport: null, // compute client side to avoid SSR serialization issues with Big
     organization: null,
     organizationVersion: null
@@ -211,7 +74,6 @@ export default {
     if (!formula) {
       return error(nuxtPageNotFound)
     }
-    const formulaVersion = formulaGetVersion(formula)
     const organization = organizationFindById(store, report.organizationId)
     if (!organization) {
       return error(nuxtPageNotFound)
@@ -221,7 +83,6 @@ export default {
       report,
       reportVersion,
       formula,
-      formulaVersion,
       organization,
       organizationVersion
     }
@@ -241,49 +102,3 @@ export default {
   }
 }
 </script>
-
-<style>
-@media print {
-  .hidden-print {
-    display: none !important;
-  }
-}
-
-.print-table-wrap {
-  overflow-x: auto;
-}
-
-.print-table {
-  border-collapse: collapse;
-}
-
-.print-table th,
-.print-table td {
-  border: 1px solid darkgray;
-  padding-left: 5px;
-  padding-right: 5px;
-}
-
-.print-table-label {
-  min-width: 20em;
-  text-align: left;
-}
-
-.print-table-amount {
-  min-width: 4em;
-  text-align: right;
-}
-
-.print-table-subheader {
-  font-style: italic;
-}
-
-.print-table-subtotal {
-  font-style: italic;
-  font-weight: bold;
-}
-
-.print-table-total {
-  font-weight: bold;
-}
-</style>
